ae35fbff1b2311680917fa780d04e6b38800b688
adrianfernanda <fernanda.adrian2@gmail.com>
2022-07-02 17:27:59 +0700

    first commit

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(bodyParser.json())
 
 const appRoute = require('./src/routes/route-smartphone');
 app.use('/', appRoute);
 
-app.listen(8080, ()=>{
-    console.log('Server Berjalan di Port : 8080'); 
+app.set('port', (process.env.PORT || 8080));
+app.listen(app.get('port'), () => {
+    console.log('Server Berjalan di Port : ', app.get('port'));
 });
+// app.listen(8080, ()=>{
+//     console.log('Server Berjalan di Port : 8080'); 
+// });
